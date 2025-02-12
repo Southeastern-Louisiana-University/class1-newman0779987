15e235e7a2018b0b49edbe795972b012b5a7c342
StaleTide <100047393+StaleTide@users.noreply.github.com>
2025-02-11 23:56:18 -0600

    Updated Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,4 @@
 package src;
-import java.util.Scanner;
 
 public class Car {
     private String make;
@@ -7,38 +6,88 @@ public class Car {
     private int year;
     private double mileage;
 
+    private double price;
+    private double gasUsed;
+    private double gasMileage;
+
+    /*
+        Overloaded method for creating the car object; one for the four data fields gone over in class and
+        one including the three new data fields. It should be noted that full functionality cannot be received
+        using the more simple method, unless the values for the datatypes are set outside of the new method.
+    */
+
     public Car(String make, String model, int year, double mileage){
         this.make = make;
         this.model = model;
         this.year = year;
         this.mileage = mileage;
     }
+
+    public Car(String make, String model, int year, double mileage, double price, double gasUsed){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+        this.gasUsed = gasUsed;
+        this.calculateGasMileage();
+    }
+
+    /*
+        Inputs the amount of miles driven in a session and prints it, as well as adding it to the total mileage and
+        printing that out as well.
+        If the gas mileage for the vehicle is calculated (done automatically with the more advanced overloaded method,
+        done manually with the simple method), the method will also print how many gallons of gas were used in this
+        drive, as well as add it to the total gas used and printing that as well.
+    */
+
     public void drive(double miles){
-        this.mileage += miles;
-        System.out.println(miles + " miles have been driven, making the total mileage now " + this.mileage);
+        this.setMileage(this.getMileage() + miles);
+        System.out.println(miles + " miles have now been driven, making the total mileage now " + this.mileage + " miles.");
+        if(this.getGasMileage() > 0){
+            this.setGasUsed(this.getGasUsed() + miles/this.getGasMileage());
+            System.out.println("Given the gas mileage of the vehicle, " + miles/this.getGasMileage() + " gallons of gas were used, making the total gas used now " + this.getGasUsed() + " gallons.");
+        }
     }
-    public String getMake(){
-        return this.make;
+
+    public void calculateGasMileage(){
+        this.gasMileage = (this.getMileage()/this.getGasUsed());
     }
-    public String getModel(){
-        return this.model;
+    public double getGasMileage(){
+        return this.gasMileage;
     }
-    public int getYear(){
-        return this.year;
-    }
-    public double getMileage(){
-        return this.mileage;
-    }
+
+    public String getMake(){return this.make;}
+    public String getModel(){return this.model;}
+    public int getYear(){return this.year;}
+    public double getPrice(){return this.price;}
+    public void setPrice(double price){this.price = price;}
+
+    public double getMileage(){return this.mileage;}
+    public void setMileage(double mileage){this.mileage = mileage;}
+
+    public double getGasUsed(){return this.gasUsed;}
+    public void setGasUsed(double gasUsed){this.gasUsed = gasUsed;}
+
+
     public void printFields(){
-        System.out.println(this.getMake());
-        System.out.println(this.getModel());
-        System.out.println(this.getYear());
-        System.out.println(this.getMileage());
+        System.out.println("Make: " + this.getMake());
+        System.out.println("Model: " + this.getModel());
+        System.out.println("Year: " + this.getYear());
+        System.out.println("Mileage: " + this.getMileage());
+
+        System.out.println("Price: $" + this.getPrice());
+        System.out.println("Gallons of Gas Used: " + this.getGasUsed());
+        System.out.println("Gas Mileage: " + this.getGasMileage());
     }
 
     public static void main(String[] args){
-        Car vehicle1 = new Car("Nissan","Rogue",2024,10);
+        Car vehicle1 = new Car("Nissan","Rogue",2024,1300);
         vehicle1.printFields();
         vehicle1.drive(50);
+
+        Car vehicle2 = new Car("Ford","F-150",2023,5000, 35000, 200);
+        vehicle2.printFields();
+        vehicle2.drive(60);
     }
 }
